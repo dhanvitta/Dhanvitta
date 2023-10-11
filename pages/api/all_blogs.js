fc0c85e790dae06185f75145412f91dc459fe89a
samanth kumar <samanthkumarsg@gmail.com>
2023-10-11 23:58:11 +0200

    API Integration

--- pages/api/all_blogs.js
+++ pages/api/all_blogs.js
@@ -1,10 +1,9 @@
 const { Client } = require('@notionhq/client');
+
 const notionAccessToken = process.env.NOTION_ACCESS_TOKEN
 const notionDatabaseId = process.env.NOTION_DATABASE_ID
 
-const client = new Client({ auth: notionAccessToken });
-
-// console.log(client)
+const notion = new Client({ auth: notionAccessToken });
 
 export default async function handler(req, res) {
 
@@ -12,11 +11,10 @@ export default async function handler(req, res) {
         throw new Error("Cannot find Notion Access Token or Notion Database Id")
     }
 
-    const query = await client.databases.query({
-        database_id: notionDatabaseId,
-    })
+    const pageId = 'c40b5d4383fc421ab9c8170922746345';
+    const response = await notion.pages.retrieve({ page_id: pageId });
 
-    const users = await client.users.list();
+    console.log(response);
 
-    res.status(200).json({ message: query.results })
+    res.status(200).json({ message: response })
 }
