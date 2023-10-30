8431773dc5e57d26596d0f6e3f56461e90705a21
samanth kumar <samanthkumarsg@gmail.com>
2023-10-30 23:28:06 +0100

    Bug fix and removed consoles

--- pages/api/all_blogs.js
+++ pages/api/all_blogs.js
@@ -18,7 +18,5 @@ export default async function handler(req, res) {
         return data.properties
     });
 
-    console.table(rows);
-
     res.status(200).json({ message: rows })
 }
--- src/app/Components/blog/tags.js
+++ src/app/Components/blog/tags.js
@@ -1,5 +1,4 @@
 export default function Tags({ data }) {
-    console.log(data)
     return (
         <div className="w-fit rounded-xl border border-purple-200 py-2 px-6 flex items-center justify-center text-xs text-purple-500 bg-purple-50"> #{data?.name} </div>
 
