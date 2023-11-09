7ab1d39f6e0d514f2b50e77d26deb4b9277f54cc
samanth kumar <samanthkumarsg@gmail.com>
2023-11-09 23:43:34 +0100

    Footer UI Fix

--- src/app/Components/Footer.js
+++ src/app/Components/Footer.js
@@ -3,16 +3,22 @@ import Link from 'next/link'
 import { FiTrendingUp } from "react-icons/fi";
 function Footer() {
     return (
-        <div className='w-full bg-slate-950 py-12 flex items-center justify-center bottom-0'>
-            <div className='w-full p-8 xs:w-full sm: md:w-1/2 lg:w-1/2 xl:w-1/2 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3  sm:py-12 md:py-12 lg:py-12 xl:py-12 items-center justify-center'>
+        <div className='w-full bg-slate-950 py-6 flex items-center justify-center bottom-0'>
+            <div className='w-full p-8 xs:w-full sm: md:w-1/2 lg:w-1/2 xl:w-1/2 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3  sm:py-12 md:py-12 lg:py-12 xl:py-12 items-start justify-center'>
                 {/*  */}
-                <div className='flex items-start flex-col'>
-                    <Link className='w-full  ' href='/'><div className='flex items-center flex-col w-28 rounded text-md font-medium p-2 text-white relative'> <FiTrendingUp className='text-xl mr-2 text-white' /> Dhanvitta <span className='text-[9px] absolute right-2 top-5'>TM</span></div></Link>
+                <div className='flex items-center flex-col w-fit'>
+                    <Link className='w-fit  ' href='/'>
+                        <div className='flex flex-col'>
+                            <div className='flex items-center flex-col w-28 rounded text-md font-medium py-2 text-white relative'> <FiTrendingUp className='text-xl mr-2 text-white' /> Dhanvitta <span className='text-[9px] absolute right-2 top-5'>TM</span></div>
+                        </div>
+                    </Link>
                     <p className='text-gray-300 text-xs font-normal mt-1'>Copyright © 2023</p>
 
                 </div>
+
+                <hr className='my-8  sm:hidden md:hidden lg:hidden xl:hidden' />
                 {/*  */}
-                <div className='my-8 sm:my-8 md:my-0 lg:my-0 xl:my-0'>
+                <div className='my-4 sm:my-4 md:my-0 lg:my-0 xl:my-0'>
                     <h1 className='text-md font-semibold text-white mb-4'>Resources</h1>
                     <div className='text-white flex flex-row sm:flex-col flex-wrap gap-3'>
                         <Link href=''><h6 className='text-sm mb-3 hover:text-yellow-300 text-gray-300'>News</h6></Link>
@@ -23,7 +29,7 @@ function Footer() {
                 </div>
                 {/*  */}
                 <div className=''>
-                    <h1 className='text-sm font-semibold text-white mb-4'>Company</h1>
+                    <h1 className='text-md font-semibold text-white mb-4'>Company</h1>
                     <div className='text-white flex flex-row sm:flex-col gap-3'>
                         <Link href=''><h6 className='text-sm mb-3 hover:text-yellow-300 text-gray-300'>About</h6></Link>
                         <Link href=''><h6 className='text-sm mb-3 hover:text-yellow-300 text-gray-300'>Careers</h6></Link>
@@ -33,7 +39,7 @@ function Footer() {
                 </div>
 
             </div>
-        </div>
+        </div >
     )
 }
 
