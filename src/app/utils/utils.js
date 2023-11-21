72c9833ec947a54f0fa034aaae680f30b09fe243
samanth kumar <samanthkumarsg@gmail.com>
2023-11-21 23:43:01 +0100

    Feature: Open Intrest data implementation

--- src/app/utils/utils.js
+++ src/app/utils/utils.js
@@ -1,7 +1,15 @@
-export async function fetcher(url) {
-    const response = await fetch(url);
-    if (!response.ok) {
-        throw new Error('Network response was not ok');
+export const fetcher = async (url) => {
+    try {
+        const response = await fetch(url);
+
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Error fetching data:', error.message);
+        throw error;
     }
-    return response.json();
-}
+};
