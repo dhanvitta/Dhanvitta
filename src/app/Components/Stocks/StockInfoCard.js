506b9c9ba5c8d6b262325633fbd89aea9652e517
samanth kumar <samanthkumarsg@gmail.com>
2024-07-29 03:57:20 +0200

    UI Updates

--- src/app/Components/Stocks/StockInfoCard.js
+++ src/app/Components/Stocks/StockInfoCard.js
@@ -3,9 +3,13 @@ import { useState, useEffect } from "react";
 import Select from 'react-select';
 import MiniCards from "./MiniCards";
 import Link from 'next/link';
+import { FiAlignRight, FiTrendingUp, FiCheck } from "react-icons/fi";
+
 
 
 function StockInfoCard({ data, index, setIndex }) {
+
+    const [isAgree, setIsAgree] = useState(false);
 
     const options = [
         { value: 'niftyoichange', label: 'Nifty' },
@@ -23,10 +27,20 @@ function StockInfoCard({ data, index, setIndex }) {
 
     return (
         <div className='-border-t bg-white p-5 border-t border-dashed flex flex-col'>
-            <div className="bg-blue-50 py-2 rounded-md text-slate-600 w-full flex flex-row items-center justify-start mb-6 text-sm border "> <span className="font-medium mx-2">Information:</span>Please Read the disclaimer prior to utilizing the OI tool for investment decision-making. |
-                <Link href='https://drive.google.com/file/d/1pJty6dMBIt1jKRf4OkCkT28MkdYvxb2a/view?usp=sharing' target="_blank"><p className='text-blue-500  mx-2 cursor-pointer'>View Disclaimer</p></Link>
 
-            </div>
+            {!isAgree ?
+                <div className="bg-blue-50 p-3 rounded-md text-slate-600 w-full flex flex-row flex-wrap items-center justify-between mb-6 text-sm border ">
+
+                    <div className="flex flex-row items-center justify-start flex-wrap">
+                        Information: Please Read the disclaimer prior to utilizing the OI tool for investment decision-making.
+                        <Link href='https://drive.google.com/file/d/1pJty6dMBIt1jKRf4OkCkT28MkdYvxb2a/view?usp=sharing' target="_blank"> <p className='text-blue-500 cursor-pointer ml-2 '> View Disclaimer</p></Link>
+                    </div>
+                    <div className="border border-blue-400 rounded-md text-blue-500 bg-blue-100 p-1 px-2 text-xs flex items-center justify-center cursor-pointer" onClick={() => setIsAgree(true)}><FiCheck className="text-blue-500 mr-2 rounded-full border border-blue-400 p-1 h-5 w-5 " /> Agree</div>
+
+                </div> :
+                ""
+            }
+
 
             <div className='row-span-2 flex-col items-start justify-start w-full top-0 sticky'>
                 <div className='pb-0'>
@@ -43,15 +57,17 @@ function StockInfoCard({ data, index, setIndex }) {
 
             </div>
 
-            {data && (
-                <div className='grid grid-cols-3 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-6 xl:grid-cols-10 mt-6 gap-6'>
-                    {Object.entries(data?.resultData?.[index] || {}).map(([key, value]) => (
-                        <MiniCards key={key} keyName={key} value={value} />
-                    ))}
+            {
+                data && (
+                    <div className='grid grid-cols-3 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-6 xl:grid-cols-10 mt-6 gap-6'>
+                        {Object.entries(data?.resultData?.[index] || {}).map(([key, value]) => (
+                            <MiniCards key={key} keyName={key} value={value} />
+                        ))}
 
-                </div>
-            )}
-        </div>
+                    </div>
+                )
+            }
+        </div >
     );
 }
 
